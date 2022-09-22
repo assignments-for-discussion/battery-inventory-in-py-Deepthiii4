bdcdbb1eef94d4891b5de787b203132ba6e749e8
Deepthiii4 <103555198+Deepthiii4@users.noreply.github.com>
2022-09-22 17:42:25 +0530

    used a for loop and if block to compare the values
    the loops are used to compare the values

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-
-def count_batteries_by_usage(cycles):
-  return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
-  }
-
-
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
-  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 3)
-  assert(counts["highCount"] == 1)
+  counts =[100, 300, 500, 600, 900, 1000]
+  low=medium=high=0
+  for i in range(0,len(counts)):
+    if counts[i]<410:
+      low+=1
+    elif counts[i]>410 and counts[i]<909:
+      medium+=1
+    else:
+      high+=1
+  print(low)
+  print(medium)
+  print(high)
   print("Done counting :)")
 
 
